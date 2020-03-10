5d871c49c37e75608cdb44856d27f76bfe0b18dd
tancaisheng <caisheng.tan@genlot.com>
2020-03-10 10:33:20 +0800

    修改生产接口地址

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export default {
     crossDomain: true, //是否开启跨域支持
-    PROD_URL: 'http://zuul.codebattery.top', //生产环境接口地址
+    PROD_URL: 'http://zuul.codebattery.top/api', //生产环境接口地址
     WS_URI: 'ws://192.168.31.68:2345',//wss://beta.vilson.xyz:2345 //WebSocket地址
     HOME_PAGE: '/home',//主页路由
 };
